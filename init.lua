c93107c8ff47441bb28827bbf249d400490e4602
Calum Mackenzie <calummackenzie2013@gmail.com>
2024-04-23 22:17:19 +0100

    Added emmet to language servers

--- init.lua
+++ init.lua
@@ -583,6 +583,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
+        emmet_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
